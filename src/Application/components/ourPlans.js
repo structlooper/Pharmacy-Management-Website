06b626939eab6c0ab37a7c2271b61089f8f8bf76
Deepak Kumar <structlooper@gmail.com>
2024-06-09 18:36:36 +0530

    search page design

---
+++ src/Application/components/ourPlans.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurPlans = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OurPlans;
--- src/Application/index.js
+++ src/Application/index.js
@@ -49,6 +49,33 @@ function App() {
                 </div>
 
             </div>
+
+            <div>
+                <div className="container py-5">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="card p-4 w-res-75 mx-auto border-2 shadow"  style={{ backgroundColor:'#F9F3FF' }}>
+                                <h1>$5 Copay</h1>
+                                <p>Top 500 generic medication plan</p>
+                                <div className="">
+                                    <button className=" me-2 me-lg-0 btn btn-orange mt-2 mt-lg-0">Get Started</button>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="card p-4 w-res-75 mx-auto border-2 shadow"  style={{ backgroundColor:'#ECF7FE' }}>
+                                <h1>$10 Copay</h1>
+                                <p>Top 2500 generic medication plan</p>
+                                <div className="">
+                                    <button className=" me-2 me-lg-0 btn btn-orange mt-2 mt-lg-0">Get Started</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     );
